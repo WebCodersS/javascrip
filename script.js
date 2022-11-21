0f528cb033a282e1f4e5e3a99d374fec8eb96193
WebCodersS <sadr0311@gmail.com>
2022-11-21 06:08:02 +0200

    my first javascript code in git hub

---
+++ script.js
@@ -0,0 +1,35 @@
+let familya = "Xolmatov";
+console.log(familya);
+let ism = "Sadriddin";
+console.log(ism);
+let age = 19;
+console.log(age);
+let bool = false;
+
+let person = {
+    name:"Sadriddin",
+    age: 19,
+    isMarried: false,
+}
+console.log(person);
+let per = {
+    name:"Akbarali",
+    age: 21,
+    isMarried: false,
+}
+console.log(per);
+let pers = {
+    name:"Sherzod",
+    age: 22,
+    isMarried: true,
+}
+console.log(pers);
+
+let arr = ["Sadriddin", "Akbarali", "Sherzod"]
+console.log(arr)
+
+arr[0] = "Temur"
+console.log(arr)
+
+alert("Hello")
+confirm("Siz ok tugmasini bosing")
